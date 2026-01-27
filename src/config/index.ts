9bdb9c6cdbdc7577f192d85e60b2efac7ce2f44f
cseswapon <cseswaponsaha@gmail.com>
2026-01-27 11:42:25 +0600

    [MOD] - chaange config file __dirname

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,6 @@
 import dotenv from "dotenv";
-import path from "path";
 
-const currentPath = path.join(__dirname, "../../.env");
-dotenv.config({
-  path: currentPath,
-});
+dotenv.config();
 
 export const config = {
   PORT: process.env.PORT,
