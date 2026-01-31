1aa3acf5c96b32abb5ba7c160269f21b13e0576f
cseswapon <cseswaponsaha@gmail.com>
2026-02-01 00:49:28 +0600

    [ADD] - added on vercel deploy command and cors frontend url added

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://foodhub-client-eight.vercel.app",
+    ],
     credentials: true,
   }),
 );
